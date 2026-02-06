e608accd179817c2eb126443ee864999cba7c974
anaorozcob <ana.orozco@correo.unimet.edu.ve>
2026-02-06 13:30:30 -0400

    Agregar estado y registros PC/Mar
    Process integra ProcessState. Se agregan PC y MAR.Cada tick consume CPU e incrementa PC/MAR.

--- src/main/java/proyecto1so/model/Process.java
+++ src/main/java/proyecto1so/model/Process.java
@@ -10,30 +10,40 @@ package proyecto1so.model;
  */
 
 
+
+
 public class Process {
 
     private final String pid;
     private final int burstTime;
     private int remainingTime;
 
-
     private final int arrivalTime;
-
 
     private Integer firstRunTick = null;
     private Integer finishTick = null;
 
-    
+    // ✅ Reglas del PDF: estados + PC/MAR
+    private ProcessState state;
+    private int pc;
+    private int mar;
+
     public Process(String pid, int burstTime) {
         this(pid, burstTime, 0);
     }
-
 
     public Process(String pid, int burstTime, int arrivalTime) {
         this.pid = pid;
         this.burstTime = burstTime;
         this.remainingTime = burstTime;
         this.arrivalTime = arrivalTime;
+
+        // Estado inicial obligatorio
+        this.state = ProcessState.NEW;
+
+        // Simplificación: arrancan en 0
+        this.pc = 0;
+        this.mar = 0;
     }
 
     public String getPid() {
@@ -60,14 +70,34 @@ public class Process {
         return finishTick;
     }
 
+    // ✅ Estado
+    public ProcessState getState() {
+        return state;
+    }
+
+    public void setState(ProcessState state) {
+        this.state = state;
+    }
+
+    // ✅ PC/MAR
+    public int getPc() {
+        return pc;
+    }
+
+    public int getMar() {
+        return mar;
+    }
+
     public boolean isFinished() {
         return remainingTime <= 0;
     }
 
-    // Consume 1 tick de CPU
+    // ✅ Consume 1 tick de CPU (1 instrucción = 1 ciclo) y aumenta PC/MAR
     public void consumeOneTick() {
         if (remainingTime > 0) {
             remainingTime--;
+            pc++;
+            mar++;
         }
     }
 
